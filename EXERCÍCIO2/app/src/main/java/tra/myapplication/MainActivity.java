76447a79778c292d4d74a15c2f0ed6bdc1ecd464
FelipeRegino <feliperegino@infojr.com.br>
2016-04-07 20:39:31 -0300

    Projeto

---
+++ EXERCÍCIO2/app/src/main/java/tra/myapplication/MainActivity.java
@@ -0,0 +1,32 @@
+package tra.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+private Button button;
+private EditText editText;
+private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        editText = (EditText) findViewById(R.id.editText);
+        textView = (TextView) findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(editText.getText().toString());
+            }
+        });
+    }
+}
